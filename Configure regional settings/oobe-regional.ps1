6ab2599e74f04bc7a932e2f6e036baafed4becd1
Stuart Crane <36999234+Stuzest@users.noreply.github.com>
2024-11-07 14:33:08 +0000

    Update oobe-regional.ps1

--- Configure regional settings/oobe-regional.ps1
+++ Configure regional settings/oobe-regional.ps1
@@ -1,14 +1,14 @@
 # configure time zone
-Get-TimeZone -ListAvailable | ?{$_.DisplayName -like "*Budapest*"} | Set-TimeZone
+Get-TimeZone -ListAvailable | ?{$_.DisplayName -like "*GMT Standard Time*"} | Set-TimeZone
 
 # configure regional/locale/keyboard settings
-# Hungarian
+# English - United Kingdom
 $region = "en-GB"
 
 Set-Culture $region
 Set-WinSystemLocale $region
-Set-WinUserLanguageList $region, "en-us" -force -wa silentlycontinue
-Set-WinHomeLocation 109
+Set-WinUserLanguageList $region, "en-GB" -force -wa silentlycontinue
+Set-WinHomeLocation 242
 
 # copy regional settings to new user accounts and welcome screen
 Copy-UserInternationalSettingsToSystem -WelcomeScreen $True -NewUser $True
